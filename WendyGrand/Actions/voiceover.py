d54aba2984505927209c99d559ac8a30f8df1341
MOSIKretto <maks.sinelnikov2007@gmail.com>
2024-04-13 19:28:57 +0300

    voiceover modified

--- WendyGrand/Actions/voiceover.py
+++ WendyGrand/Actions/voiceover.py
@@ -1,8 +1,23 @@
 from playsound import playsound
 from config import AUDIO_DIR
-
+from random import *
 
 class ActionsVoiceover:
+
     @staticmethod
     def browser_voiceover():
-        playsound(AUDIO_DIR + "BrowserOpen.mp3")
+        
+        executive_voice = randint(0, 5)
+
+        if executive_voice == 0:
+            playsound(AUDIO_DIR + "BrowserOpen.mp3")
+        elif executive_voice == 1:
+            playsound(AUDIO_DIR + 'Doing.mp3')
+        elif executive_voice == 2:
+            playsound(AUDIO_DIR + 'OneMoment.mp3')
+        elif executive_voice == 3:
+            playsound(AUDIO_DIR + 'Second.mp3')
+        elif executive_voice == 4:
+            playsound(AUDIO_DIR + 'OpeningBrowser.mp3')
+        else:
+            playsound(AUDIO_DIR + "OpenItNow.mp3")
