f50d66c81d5e3343c9a8c9fe83be737e51710c2e
MOSIKretto <maks.sinelnikov2007@gmail.com>
2024-04-24 22:18:48 +0300

    fix BrowserVoice

--- WendyGrand/AudioHelper/BrowserVoice.py
+++ WendyGrand/AudioHelper/BrowserVoice.py
@@ -9,18 +9,22 @@ from playsound import playsound
 from config import AUDIO_DIR
 from random import *
 
-        
-executive_voice = randint(0, 5)
+class ActionsVoiceover:
 
-if executive_voice == 0:
-    playsound(AUDIO_DIR + "BrowserOpen.mp3")
-elif executive_voice == 1:
-    playsound(AUDIO_DIR + 'Doing.mp3')
-elif executive_voice == 2:
-    playsound(AUDIO_DIR + 'OneMoment.mp3')
-elif executive_voice == 3:
-    playsound(AUDIO_DIR + 'Second.mp3')
-elif executive_voice == 4:
-    playsound(AUDIO_DIR + 'OpeningBrowser.mp3')
-else:
-    playsound(AUDIO_DIR + "OpenItNow.mp3")
+    @staticmethod
+    def browser_voiceover():
+        
+        executive_voice = randint(0, 5)
+
+        if executive_voice == 0:
+            playsound(AUDIO_DIR + "BrowserOpen.mp3")
+        elif executive_voice == 1:
+            playsound(AUDIO_DIR + 'Doing.mp3')
+        elif executive_voice == 2:
+            playsound(AUDIO_DIR + 'OneMoment.mp3')
+        elif executive_voice == 3:
+            playsound(AUDIO_DIR + 'Second.mp3')
+        elif executive_voice == 4:
+            playsound(AUDIO_DIR + 'OpeningBrowser.mp3')
+        else:
+            playsound(AUDIO_DIR + "OpenItNow.mp3")
