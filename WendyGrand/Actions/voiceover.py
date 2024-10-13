e54bba9aa438fe707cbfe5bcefc806f38a884c1d
Max <149097230+MOSIKretto@users.noreply.github.com>
2024-10-13 14:52:28 +0300

    fix small problem :)

--- WendyGrand/Actions/voiceover.py
+++
@@ -1,8 +0,0 @@
-from playsound import playsound
-from config import AUDIO_DIR
-
-
-class ActionsVoiceover:
-    @staticmethod
-    def browser_voiceover():
-        playsound(AUDIO_DIR + "BrowserOpen.mp3")
