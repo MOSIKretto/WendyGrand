811b8bd3c138503ee2094bf6b98b964c998bbaa2
Max <149097230+MOSIKretto@users.noreply.github.com>
2024-10-14 20:09:24 +0300

    временные изменения

--- GitInstaller.py
+++ GitInstaller.py
@@ -1,4 +1,12 @@
+import subprocess
+
 def InstallGit(manager):
     print(manager[0], type(manager[0]))
     if manager[0] == 'pacman':
-        pass
+        try:
+            # Запуск команды pacman для установки git
+            subprocess.run(["sudo", "pacman", "-S", "git"], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Ошибка установки Git: {e}")
+
+
