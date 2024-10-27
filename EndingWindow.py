205bce84a68e555b059cdfed65da42129d3a2aed
Max <149097230+MOSIKretto@users.noreply.github.com>
2024-10-28 01:09:29 +0300

    Small fix EndingWindow

--- EndingWindow.py
+++ EndingWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QDesktopWidget
 from PyQt5.QtCore import Qt
 import subprocess
 import GitRemuver
@@ -8,26 +8,35 @@ class End(QWidget):
 
     #Отрисовка окна
     def __init__(self):
-
         super().__init__()
-        self.setFixedSize(450,100)
+        self.setFixedSize(650, 250)
         self.setWindowTitle("Wendy's installer")
         self.setStyleSheet("background-color: black;")
         self.startPos = None
         self.isDragging = False
-        layout = QVBoxLayout()
-        self.label = QLabel("Wendy успешно установлена, можете начать использование!", self)
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label.setStyleSheet("color: white;")
+
+        button_layout = QHBoxLayout()
+        self.retry_btn = QPushButton("Попробовать ещё раз", self)
+        self.retry_btn.setStyleSheet("background-color: white;")
+        self.retry_btn.clicked.connect(self.retry_install)
+        button_layout.addWidget(self.retry_btn)
+
         self.close_btn = QPushButton("Выйти из установщика", self)
         self.close_btn.setStyleSheet("background-color: white;")
         self.close_btn.clicked.connect(self.closing)
+        button_layout.addWidget(self.close_btn)
+
+        layout = QVBoxLayout()
+        self.label = QLabel("Wendy успешно установлена, можете начать использование!\n \n \n Warning!!!\n Если это окно появилось моментально и папка ''WendyGrand'' не была создалась,\n возможно, у вас проблемы с интернетом или не удалось подключится к GitHub.\n\n Попробуйте еще раз!", self)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setStyleSheet("color: white;")
+
         layout.addWidget(self.label)
-        layout.addWidget(self.close_btn)
+        layout.addLayout(button_layout)
         self.setLayout(layout)
         self.center()
 
-    #Централизовать окно
+
     def center(self):
 
         qr = self.frameGeometry()
@@ -35,7 +44,7 @@ class End(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-    #Передвижение окна
+    
     def mousePressEvent(self, event):
 
         if event.button() == Qt.LeftButton:
@@ -54,7 +63,6 @@ class End(QWidget):
             self.isDragging = False
             self.startPos = None
 
-    #Закрываем установщик
     def closing(self):
 
         try:
@@ -65,11 +73,10 @@ class End(QWidget):
                 self.check_and_remove_git()
                 sys.exit(0)
 
-        except:        
-                print("Git не будет удален.")
-                sys.exit(0)
+        except:    
+            print("Git не будет удален.")
+            sys.exit(0)
 
-    #Проверка и удаление git в системе
     def check_and_remove_git(self):
 
         package_managers = ["pacman", "apt", "dnf", "yum"]
@@ -82,16 +89,20 @@ class End(QWidget):
         if installed_managers:
             GitRemuver.RemuverGit(installed_managers)
 
-        try:
-            subprocess.run([manager, '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-            return True
-        
-        except subprocess.CalledProcessError:
-            return False
-        
-        except FileNotFoundError:
-            return False
+        if installed_managers:
+            try:
+                subprocess.run([installed_managers[0], '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+                return True
 
+            except subprocess.CalledProcessError:
+                return False
+
+            except FileNotFoundError:
+                return False
+
+    def retry_install(self):
+        subprocess.run(["java", "Translator.java", "ProgressBar.py"])
+        sys.exit(0)
 
 app = QApplication(sys.argv)
 window = End()
