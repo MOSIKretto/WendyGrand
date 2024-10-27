6bf9ae5a444750b181b5c1f0eb9fea1636bcda5b
Viacheslav <149095520+VenTurchik@users.noreply.github.com>
2024-10-27 13:19:33 +0300

    + animation in progressbar

--- ProgressBar.py
+++ ProgressBar.py
@@ -1,9 +1,50 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QDesktopWidget
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5 import QtCore
 import subprocess
 import sys
 import time  # Временная хрень
 
+ascii_picture_one = r"""
+@~~~
+@  ~
+@~~~
+"""
+ascii_picture_two = r"""
+~~~~
+@  ~
+@@~~
+"""
+ascii_picture_three = r"""
+~~~~
+~  ~
+@@@~
+"""
+ascii_picture_four = r"""
+~~~~
+~  @
+~~@@
+"""
+ascii_picture_five = r"""
+~~~@
+~  @
+~~~@
+"""
+ascii_picture_six = r"""
+~~@@
+~  @
+~~~~
+"""
+ascii_picture_seven = r"""
+~@@@
+~  ~
+~~~~
+"""
+ascii_picture_eight = r"""
+@@~~
+@  ~
+~~~~
+"""
 
 class CloneThread(QThread):
     finished = pyqtSignal()
@@ -16,6 +57,13 @@ class CloneThread(QThread):
         time.sleep(5)  # Временная хрень
         subprocess.run(["java", "Translator.java", "EndingWindow.py"])
 
+class ProgressHandler(QtCore.QThread): # Для анимации загрузки
+    signal = QtCore.pyqtSignal(int)
+    def run(self):
+        for step in range(1,9):
+            self.signal.emit(step)
+            time.sleep(0.1)
+        self.run()
 
 class Progress(QWidget):
 
@@ -24,22 +72,51 @@ class Progress(QWidget):
 
         super().__init__()
         self.setWindowTitle("Wendy's cloning...")
-        self.setFixedSize(350, 75)
+        self.setFixedSize(300, 100)
         self.startPos = None
         self.isDragging = False
         self.layout = QVBoxLayout(self)
         self.setStyleSheet("background-color: black;")
+        self.art = QLabel('', self)
+        self.art.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.art.setStyleSheet("color: white;")
         self.label = QLabel(self)
         self.label.setText("Копирование репозитория...")
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.label)
         self.label.setStyleSheet("color: white;")
+        self.layout.addWidget(self.art)
         self.center()
 
         # Запуск потока
         self.clone_thread = CloneThread()
+        self.handler = ProgressHandler()
+        self.handler.signal.connect(self.start_animation)
         self.clone_thread.finished.connect(self.close)
         self.clone_thread.start()
+        self.handler.start()
+
+
+    def start_animation(self, step):
+        match step:
+            case 1:
+                self.art.setText(f"<pre style='font-family:Courier; background-color: black; font-size:12pt; color: #FFFFFF;'>{ascii_picture_one}</pre")
+            case 2:
+                self.art.setText(f"<pre style='font-family:Courier; background-color: black; font-size:12pt; color: #FFFFFF;'>{ascii_picture_two}</pre")
+            case 3:
+                self.art.setText(f"<pre style='font-family:Courier; background-color: black; font-size:12pt; color: #FFFFFF;'>{ascii_picture_three}</pre")
+            case 4:
+                self.art.setText(f"<pre style='font-family:Courier; background-color: black; font-size:12pt; color: #FFFFFF;'>{ascii_picture_four}</pre")
+            case 5:
+                self.art.setText(f"<pre style='font-family:Courier; background-color: black; font-size:12pt; color: #FFFFFF;'>{ascii_picture_five}</pre")
+            case 6:
+                self.art.setText(f"<pre style='font-family:Courier; background-color: black; font-size:12pt; color: #FFFFFF;'>{ascii_picture_six}</pre")
+            case 7:
+                self.art.setText(f"<pre style='font-family:Courier; background-color: black; font-size:12pt; color: #FFFFFF;'>{ascii_picture_seven}</pre")
+            case 8:
+                self.art.setText(f"<pre style='font-family:Courier; background-color: black; font-size:12pt; color: #FFFFFF;'>{ascii_picture_eight}</pre")
+            case other:
+                self.art.setText("OUTPUT ART ERROR!!!")
 
     # Централизовать окно
     def center(self):
