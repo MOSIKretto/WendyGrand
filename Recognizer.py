d84478d7b4a2ea80d1602d5e07ed72a52fa99405
Max <149097230+MOSIKretto@users.noreply.github.com>
2024-11-01 00:35:38 +0300

    Recognizer++

--- Recognizer.py
+++ Recognizer.py
@@ -13,22 +13,21 @@ import vosk
 import sounddevice as sd
 import queue
 import subprocess
+import time
 import sys
 import os
 
-
 ActionsVoiceover.HelloVoiceover()
-
 
 q = queue.Queue()
 model = vosk.Model('model_small')
 device = sd.default.device
 samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate'])
-
+last_comand = ""
+command_timer = 0
 
 def callback(indata, frames, time, status):
     q.put(bytes(indata))
-
 
 with sd.RawInputStream(samplerate=samplerate, blocksize = 16000, device=device[0], dtype='int16', 
                        channels=1, callback=callback):
@@ -39,17 +38,21 @@ with sd.RawInputStream(samplerate=samplerate, blocksize = 16000, device=device[0
             if rec.AcceptWaveform(data):
                 text = rec.Result()[14:-3]
                 #text = str(input()) #Тесты с клавиатуры
-                if (text.startswith("венди пока")) or (text.startswith("среда пока")) or (text.startswith("вэнди пока")):
+                if text.startswith(("венди пока", "среда пока", "вэнди пока")):
                     print("Recognizer:", text)
                     ActionsVoiceover.ByeVoiceover()
                     subprocess.run(["pkill", "glava"])
                     sys.exit(0)
-                elif (text.startswith("венди")) or (text.startswith("среда")) or (text.startswith("вэнди")):
+                elif text.startswith(("венди", "среда", "вэнди")):
                     print("Recognizer:", text)
-                    text = text.replace("венди", "")
-                    text = text.replace("среда", "")
-                    text = text.replace("вэнди", "")
-                    subprocess.run(["java", "Java_Dictionary.java", text.strip()], stdout=sys.stdout, 
-                                stderr=sys.stdout, cwd=os.getcwd())
-                else:
-                    pass
+                    last_comand = text
+                    command_timer = time.time()
+                    text = text.replace("венди", "").replace("среда", "").replace("вэнди", "")
+                    subprocess.run(["java", "Java_Dictionary.java", text.strip()],
+                                   stdout=sys.stdout, stderr=sys.stdout, cwd=os.getcwd())
+                elif (last_comand != "") and (text != "") and (time.time() - command_timer <= 10):
+                    print("Recognizer:", text)
+                    subprocess.run(["java", "Java_Dictionary.java", text.strip()],
+                                   stdout=sys.stdout, stderr=sys.stdout, cwd=os.getcwd())
+                    last_command = ""
+                     
