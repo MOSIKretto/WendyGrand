f6b810391f4853f35d1db61cf8969d251f84f1bb
Max <149097230+MOSIKretto@users.noreply.github.com>
2024-11-12 01:27:13 +0300

    fix

---
+++ Modules/YourModules/creator.cpp
@@ -0,0 +1,9 @@
+ #include <stdlib.h>
+
+ int main()
+ {
+    system("xdg-open https://t.me/TheElexum");
+    return 0;
+ }
+   
+
--- runModules.py
+++ runModules.py
@@ -1,5 +1,7 @@
 import subprocess
 import sys
+import os
+
 
 def slice_until_period(input_string):
     period_index = input_string.rfind('.')
@@ -9,9 +11,20 @@ def slice_until_period(input_string):
         print('К сожалению, я не могу найти точку в строке')
         return None
 
+# Для C/C++
+def compile_and_run_cpp(source_file):
+    base_name = os.path.splitext(os.path.basename(source_file))[0]
+    directory = os.path.dirname(source_file)
+    executable_path = os.path.join(directory, base_name)
+    subprocess.run(["g++", source_file, "-o", executable_path])
+    subprocess.run([f"./{executable_path}"])
+
+
 res = slice_until_period(str(sys.argv[1]))
 
 if res == "py":
-    subprocess.run(["python3", f"../WendyGrand/Modules/YourModules/{str(sys.argv[1])}"])
-else:
-    subprocess.run([slice_until_period(str(sys.argv[1])), f"../WendyGrand/Modules/YourModules/{str(sys.argv[1])}"])
+    subprocess.run(["python3", "../WendyGrand/Modules/YourModules/" + sys.argv[1]])
+elif res == "cpp" or res == "c":
+    compile_and_run_cpp("../WendyGrand/Modules/YourModules/" + sys.argv[1])
+elif res == "java":
+    subprocess.run([res, "../WendyGrand/Modules/YourModules/" + sys.argv[1]])
