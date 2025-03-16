1123922e8968560fe764b086a54c88429c166fa5
Viacheslav <149095520+VenTurchik@users.noreply.github.com>
2025-03-16 04:33:31 +0300

    preparing to update window

--- GUI/TabsWindow.py
+++ GUI/TabsWindow.py
@@ -1,10 +1,10 @@
-from PyQt6.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QLabel, QWidget, QPushButton, QCheckBox, QListWidget, QFrame, QSystemTrayIcon, QMenu
+from PyQt6.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QLabel, QWidget, QPushButton, QCheckBox, QListWidget, QFrame, QLineEdit
 from PyQt6.QtCore import Qt, QPoint
-from PyQt6.QtGui import QPixmap, QIcon
+from PyQt6.QtGui import QPixmap
 import sys
 import os
 
-list_item = ['Начало', 'Зависимоти', 'Установка', 'Конец']
+list_item = ['Wendy', 'Настройка', 'Создание модуля', 'Техничка']
 
 class Installer(QWidget):
     def __init__(self):
@@ -18,84 +18,15 @@ class Installer(QWidget):
         self.isDragging = False
 
         # Косметические настройки
-        self.setWindowTitle("Wendy")
+        self.setWindowTitle("Wendy's Installer")
         self.setFixedSize(700, 400)
         self.setStyleSheet('background-color: #241B2C;')
-        self.setWindowIcon(QIcon('../WendyGrand/GUI/icon.ico'))
 
         # Основной layout окна
         main_layout = QHBoxLayout()
 
-        #-------------------------------------------------------------------------------------------------------
-        # Настройка фреймов для этапов
-
-        # Начало
-        begin_frame = QFrame(self)
-        begin_frame.setFrameShape(QFrame.Shape.Box)
-        begin_frame.setFrameShadow(QFrame.Shadow.Sunken)
-        begin_frame.setStyleSheet("""
-            QFrame {
-                background-color: #1A1A1D; 
-                color: #E1D7C6; 
-                border: 2px solid #E1D7C6; 
-                border-radius: 10px; 
-                padding: 10px;
-            }
-        """)
-        begin_frame.setVisible(True)
-
-        # Зависимости
-        adiction_frame = QFrame(self)
-        adiction_frame.setFrameShape(QFrame.Shape.Box)
-        adiction_frame.setFrameShadow(QFrame.Shadow.Sunken)
-        adiction_frame.setStyleSheet("""
-            QFrame {
-                background-color: #1A1A1D; 
-                color: #E1D7C6; 
-                border: 2px solid #E1D7C6; 
-                border-radius: 10px; 
-                padding: 10px;
-            }
-        """)
-        adiction_frame.setVisible(False)
-
-        # Установка
-        install_frame = QFrame(self)
-        install_frame.setFrameShape(QFrame.Shape.Box)
-        install_frame.setFrameShadow(QFrame.Shadow.Sunken)
-        install_frame.setStyleSheet("""
-            QFrame {
-                background-color: #1A1A1D; 
-                color: #E1D7C6; 
-                border: 2px solid #E1D7C6; 
-                border-radius: 10px; 
-                padding: 10px;
-            }
-        """)
-        install_frame.setVisible(False)
-
-        # Конец
-        final_frame = QFrame(self)
-        final_frame.setFrameShape(QFrame.Shape.Box)
-        final_frame.setFrameShadow(QFrame.Shadow.Sunken)
-        final_frame.setStyleSheet("""
-            QFrame {
-                background-color: #1A1A1D; 
-                color: #E1D7C6; 
-                border: 2px solid #E1D7C6; 
-                border-radius: 10px; 
-                padding: 10px;
-            }
-        """)
-        final_frame.setVisible(False)
-
-        
-
-
-
-
         # Настройка списка этапов
-        self.stage_list_layout = QVBoxLayout(self)
+        self.stage_list_layout = QVBoxLayout()
         self.stage_list = QListWidget(self)
         self.stage_list.setStyleSheet("""
             QListWidget {
@@ -126,41 +57,112 @@ class Installer(QWidget):
         self.stage_list.itemClicked.connect(self.select_stage)
         main_layout.addLayout(self.stage_list_layout)
 
+        # Настройка фреймов для этапов
+        self.frames = []
+        self.frames.append(self.create_frame("Wendy"))
+        self.frames.append(self.create_frame("Настройки"))
+        self.frames.append(self.create_frame("Создание модуля"))
+        self.frames.append(self.create_frame("Техничка"))
 
-        
-        
-        
-        
+        # Добавляем фреймы в основной layout
+        self.frame_layout = QVBoxLayout()
+        for frame in self.frames:
+            self.frame_layout.addWidget(frame)
+        main_layout.addLayout(self.frame_layout)
+
         self.setLayout(main_layout)
-        
 
+        # Показываем первый фрейм по умолчанию
+        self.frames[0].setVisible(True)
+
+    def create_frame(self, title):
+        frame = QFrame(self)
+        frame.setFrameShape(QFrame.Shape.Box)
+        frame.setFrameShadow(QFrame.Shadow.Sunken)
+        frame.setStyleSheet("""
+            QFrame {
+                background-color: #1A1A1D; 
+                color: #E1D7C6; 
+                border: 2px solid #E1D7C6; 
+                border-radius: 10px; 
+                padding: 10px;
+            }
+        """)
+        frame.setVisible(False)
+
+        label = QLabel(title, frame)
+        label.setStyleSheet("""
+            QLabel {
+                color: #E1D7C6;
+            }
+        """)
+        # Добавляем дополнительные виджеты в каждый фрейм
+        if title == "Начало":
+            
+
+            description_label = QLabel("""
+            Спасибо, что решили установить Wendy!
+            """,frame)
+            description_label.setStyleSheet("""
+                QLabel {
+                   color: #E1D7C6;
+                }
+            """)
+
+        
+        if title == "Установка":
+            input_line_edit = QLineEdit(frame)
+            input_line_edit.setStyleSheet("""
+                QLineEdit {
+                    background-color: #1A1A1D; 
+                    color: #E1D7C6; 
+                    border: 2px solid #E1D7C6; 
+                    border-radius: 5px; 
+                    padding: 5px;
+                }
+            """)
+            layout = QVBoxLayout()
+            layout.addWidget(label)
+            #layout.addWidget(description_label)
+            layout.addWidget(input_line_edit)
+        else:
+            layout = QVBoxLayout()
+            layout.addWidget(label)
+          #  layout.addWidget(description_label)
+
+        frame.setLayout(layout)
+
+        return frame
 
     def select_stage(self, item):
-        pass
-    #Централизуем окно
-    def center(self):
+        # Скрываем все фреймы
+        for frame in self.frames:
+            frame.setVisible(False)
 
+        # Показываем фрейм, соответствующий выбранному элементу
+        index = self.stage_list.row(item)
+        self.frames[index].setVisible(True)
+
+    # Централизуем окно
+    def center(self):
         qr = self.frameGeometry()
         screen = QApplication.primaryScreen()
         cp = screen.availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-    #Передвижение окна
+    # Передвижение окна
     def mousePressEvent(self, event):
-
         if event.button() == Qt.MouseButton.LeftButton:
             self.isDragging = True
-            self.startPos = event.position().toPoint()  
+            self.startPos = event.position().toPoint()
 
     def mouseMoveEvent(self, event):
-
         if self.isDragging:
-            delta = event.position().toPoint() - self.startPos  
-            self.move(self.pos() + delta)  
+            delta = event.position().toPoint() - self.startPos
+            self.move(self.pos() + delta)
 
     def mouseReleaseEvent(self, event):
-
         if event.button() == Qt.MouseButton.LeftButton:
             self.isDragging = False
             self.startPos = None
@@ -169,20 +171,4 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Installer()
     window.show()
-
-    tray_icon = QSystemTrayIcon(QIcon('../WendyGrand/GUI/icon.ico'), parent=app)
-    menu = QMenu()
-
-    exit_action = menu.addAction("Закрыть Wendy")
-    exit_action.triggered.connect(app.quit)
-
-    settings_action = menu.addAction("Открыть Настройки")
-    #settings_action.triggered.connect(window.settings)
-
-    settings_action = menu.addAction("Открыть Venv creator")
-    #settings_action.triggered.connect(window.edit_venv)
-    
-    tray_icon.setContextMenu(menu)
-    tray_icon.setVisible(True)
-
     sys.exit(app.exec())
